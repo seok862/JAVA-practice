3dc0ee504d22a9544fd18d788306d5da393dc53a
SJkim <tjrwn1247@gmail.com>
2022-02-08 18:19:55 +0900

    baekjoon bronze-5 17256

--- baekjoon/bronze 5/17256/src/Main.java
+++ baekjoon/bronze 5/17256/src/Main.java
@@ -2,14 +2,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a1 = sc.nextInt();
-		int a2 = sc.nextInt();
-		int a3 = sc.nextInt();
-		
-		int c1 = sc.nextInt();
-		int c2 = sc.nextInt();
-		int c3 = sc.nextInt();
-		
-		System.out.printf("%d"+" "+"%d"+" "+"%d", c1-a3, c2/a2, c3-a1);
+		int n = sc.nextInt();
+		System.out.println(1);
+		System.out.println(0);
 	}
 }
