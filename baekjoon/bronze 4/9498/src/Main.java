c13f953b02845613c3ee059ab5543fc61db8c1e2
SJkim <tjrwn1247@gmail.com>
2022-02-15 23:28:55 +0900

    baekjoon bronze-4 9498

---
+++ baekjoon/bronze 4/9498/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        
+        if(90 <= A && A <= 100){
+            System.out.println("A");
+        } else if(80 <= A && A < 90){
+            System.out.println("B");
+        } else if(70 <= A && A < 80){
+            System.out.println("C");
+        } else if(60 <= A && A < 70){
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    } 
+}
