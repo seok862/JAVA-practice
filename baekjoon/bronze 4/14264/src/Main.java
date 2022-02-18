942beaf93fa29d1ab1f9dd90eeac6c199a525f19
SJkim <tjrwn1247@gmail.com>
2022-02-18 22:54:10 +0900

    baekjoon bronze-4 14264

---
+++ baekjoon/bronze 4/14264/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		double area = (Math.sqrt(3) / 4) * Math.pow(L, 2);
+		System.out.println(area);
+	}
+}
