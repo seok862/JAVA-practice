11b91e59a70573d99d1981716efb4158cef82f88
SJkim <tjrwn1247@gmail.com>
2022-02-21 13:40:17 +0900

    baekjoon bronze-4 15726

---
+++ baekjoon/bronze 4/15726/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		 
+		System.out.println(Math.max((int)(A * B / C), (int)(A / B * C)));
+	}
+}
