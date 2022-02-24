d138e7dde40837f1f35b5a62b88563826c212ac4
SJkim <tjrwn1247@gmail.com>
2022-02-24 21:30:09 +0900

    baekjoon bronze-4 15963

--- baekjoon/bronze 4/15963/src/Main.java
+++ baekjoon/bronze 4/15963/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		long N = sc.nextInt();
-		long M = sc.nextInt();
+		long N = sc.nextLong();
+		long M = sc.nextLong();
 		
 		if(N==M) {
 			System.out.println(1);
