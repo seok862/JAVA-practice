5b2ceb641f033a46b1bad89f247549769a673130
SJkim <tjrwn1247@gmail.com>
2022-02-27 17:52:50 +0900

    baekjoon bronze-4 17362

---
+++ baekjoon/bronze 4/17362/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N%8 == 1) {
+			System.out.println(1);
+		} else if(N%8 == 2 || N%8 == 0) {
+			System.out.println(2);
+		} else if(N%8 == 3 || N%8 == 7) {
+			System.out.println(3);
+		} else if(N%8 == 4 || N%8 == 6) {
+			System.out.println(4);
+		} else {
+			System.out.println(5);
+		}
+		
+	}
+}
