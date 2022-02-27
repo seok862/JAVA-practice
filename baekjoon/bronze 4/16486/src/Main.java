9d7179c8fbf16b9bbf2952f9d5ef10198536cd6a
SJkim <tjrwn1247@gmail.com>
2022-02-27 17:28:42 +0900

    baekjoon bronze-4 16486

---
+++ baekjoon/bronze 4/16486/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int d1 = sc.nextInt();
+		int d2 = sc.nextInt();
+		double pi = 3.141592;
+		System.out.println((2*pi*d2)+2*d1);
+	}
+}
