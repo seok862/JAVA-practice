d691422c268c9c115a01f385615a65e521674c78
SJkim <tjrwn1247@gmail.com>
2022-04-08 18:04:06 +0900

    D1 2063

---
+++ SW Expert Academy/D1/2063/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[N/2]);
+	}
+}
