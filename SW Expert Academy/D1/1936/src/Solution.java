0c95d4790e813dc9fb2c0c9526379b8d4ba5af5e
SJkim <tjrwn1247@gmail.com>
2022-04-08 19:43:21 +0900

    D1 1936

---
+++ SW Expert Academy/D1/1936/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A == 1) {
+			if(B == 2) {
+				System.out.println("B");
+			} else if(B == 3) {
+				System.out.println("A");
+			}
+		}
+		if(A == 2) {
+			if(B == 1) {
+				System.out.println("A");
+			} else if(B == 3) {
+				System.out.println("B");
+			}
+		}
+		if(A == 3) {
+			if(B == 1) {
+				System.out.println("B");
+			} else if(B == 2) {
+				System.out.println("A");
+			}
+		}
+	}
+}
