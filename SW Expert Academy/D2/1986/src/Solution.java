9390c9e5a5875b9d1e820d5a5a4d9eb555638e90
SJkim <tjrwn1247@gmail.com>
2022-04-10 09:09:14 +0900

    D2 1986

---
+++ SW Expert Academy/D2/1986/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			int sum = 0;
+			
+			for(int i=1; i<=N; i++) {
+				
+				if(i%2 == 0) {
+					sum += -i;
+				} else {
+					sum += i;
+				}
+			}
+			System.out.println("#"+t+" "+sum);
+		}
+	}
+}
