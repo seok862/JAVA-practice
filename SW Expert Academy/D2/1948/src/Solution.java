d2bdfab54b9a033d611604f10f098462fad1df04
SJkim <tjrwn1247@gmail.com>
2022-04-11 20:04:41 +0900

    D2 1948

---
+++ SW Expert Academy/D2/1948/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t=1; t<=T; t++){
+			int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			
+			int sum = 0;
+			int sum2 = 0;
+			
+			for(int i=0; i<a; i++) {
+				sum += arr[i];
+			}
+			sum = sum+b;
+			
+			for(int i=0; i<c; i++) {
+				sum2 += arr[i];
+			}
+			sum2 = sum2+d;
+			
+			System.out.println("#"+t+" "+(sum2-sum+1));
+		}
+	}
+}
