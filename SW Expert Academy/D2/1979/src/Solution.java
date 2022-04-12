d435df94daa1320e964d9b34db144e270d465bc5
SJkim <tjrwn1247@gmail.com>
2022-04-12 18:32:46 +0900

    D2 1979

--- SW Expert Academy/D2/1979/src/Solution.java
+++ SW Expert Academy/D2/1979/src/Solution.java
@@ -1,61 +1,59 @@
 import java.util.Scanner;
+
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
-		for(int t=1; t<=T; t++) {
+
+		for (int t = 1; t <= T; t++) {
 			int N = sc.nextInt();
 			int K = sc.nextInt();
-			
+
 			int[][] arr = new int[N][N];
 			int ans = 0;
-			
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
 					arr[i][j] = sc.nextInt();
 				}
 			}
-			
-			for(int i=0; i<N; i++) {
+
+			for (int i = 0; i < N; i++) {
 				int colCheck = 0;
-				for(int j=0; j<N; j++) {
-					if(arr[i][j] == 0) {
-						if(colCheck == K) {
+				for (int j = 0; j < N; j++) {
+					if (arr[i][j] == 0) {
+						if (colCheck == K) {
 							ans++;
 						}
 						colCheck = 0;
-					}
-					else {
+					} else {
 						colCheck += 1;
 					}
-					
+
 				}
-				if(colCheck == K) {
+				if (colCheck == K) {
 					ans++;
 				}
 			}
-			
-			for(int i=0; i<N; i++) {
+
+			for (int i = 0; i < N; i++) {
 				int rowCheck = 0;
-				for(int j=0; j<N; j++) {
-					if(arr[j][i] == 0) {
-						if(rowCheck == K) {
+				for (int j = 0; j < N; j++) {
+					if (arr[j][i] == 0) {
+						if (rowCheck == K) {
 							ans++;
 						}
 						rowCheck = 0;
-					}
-					else {
+					} else {
 						rowCheck += 1;
 					}
-				
-					}
-				if(rowCheck == K) {
-					ans ++;
+
 				}
-				
+				if (rowCheck == K) {
+					ans++;
+				}
 			}
-			System.out.println("#"+t+" "+ans);
+			System.out.println("#" + t + " " + ans);
 		}
 	}
 }
