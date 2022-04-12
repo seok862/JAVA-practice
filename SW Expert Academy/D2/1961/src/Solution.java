084d18b15d01a2eb83953d581ed85033e4d54325
SJkim <tjrwn1247@gmail.com>
2022-04-12 18:32:54 +0900

    D2 1961

---
+++ SW Expert Academy/D2/1961/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			
+			int[][] arr = new int[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			
+		}
+	}
+}
