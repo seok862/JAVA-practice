a5c019a06e07e1b97831b9b649d993014fabc3cb
SJkim <tjrwn1247@gmail.com>
2022-04-12 21:24:02 +0900

    D1 2070

--- SW Expert Academy/D1/2070/src/Solution.java
+++ SW Expert Academy/D1/2070/src/Solution.java
@@ -3,16 +3,19 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		for(int i=0; i<T; i++) {
-			int num1 = sc.nextInt();
-			int num2 = sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
 			
-			if(num1 > num2) {
-				System.out.println("#"+(i+1)+" >");
-			} else if(num1 < num2) {
-				System.out.println("#"+(i+1)+" <");
-			} else {
-				System.out.println("#"+(i+1)+" =");
+			if(A==B) {
+				System.out.println("#"+t+" =");
+			}
+			else if(A>B) {
+				System.out.println("#"+t+" >");
+			}
+			else {
+				System.out.println("#"+t+" <");
 			}
 		}
 	}
