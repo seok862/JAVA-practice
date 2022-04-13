c11edb96123db68cf5acf18bc9cd2df7870c4e33
SJkim <tjrwn1247@gmail.com>
2022-04-13 22:03:01 +0900

    D2 1285

--- SW Expert Academy/D2/1285/src/Solution.java
+++ SW Expert Academy/D2/1285/src/Solution.java
@@ -6,32 +6,23 @@ public class Solution {
 		
 		for(int t=1; t<=T; t++) {
 			int N = sc.nextInt();
-			int ans = 100000;
+			
+			int min = 100000;
 			int cnt = 0;
 			for(int i=0; i<N; i++) {
 				int a = sc.nextInt();
 				
-				if(a>0) {
-					if(ans > a) {
-						ans = a;
-						cnt = 1;
-					}
-					if(ans == a) {
-						cnt++;
-					}
-				}
-				if(a<0) {
-					if(ans > -a) {
-						ans = a;
-						cnt = 1;
-					}
-					if(ans == a) {
-						cnt++;
-					}
+				if(Math.abs(a)<min) {
+					min = Math.abs(a);
+					cnt = 1;
+					
 				}
 				
+				else if(Math.abs(a) == min) {
+					cnt++;
+				}			
 			}
-			System.out.println(ans+" "+cnt);
+			System.out.println("#"+t+" "+min+" "+cnt);
 		}
 	}
 }
