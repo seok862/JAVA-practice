5c0a5c4848541d8cf0fb7d281d2b9419eee3edb7
SJkim <tjrwn1247@gmail.com>
2022-04-13 21:40:29 +0900

    D2 1974 다른방법

--- SW Expert Academy/D2/1974/src/Solution.java
+++ SW Expert Academy/D2/1974/src/Solution.java
@@ -6,44 +6,65 @@ public class Solution {
 		
 		for(int t=1; t<=T; t++) {
 			int[][] arr = new int[9][9];
-			int ans = 1;
+			boolean sign = true;
+			
 			for(int i=0; i<9; i++) {
-				int sum = 0;
-				
 				for(int j=0; j<9; j++) {
 					arr[i][j] = sc.nextInt();
-					sum += arr[i][j];
-				}
-				
-				if(sum != 45) {
-					ans = 0;
 				}
 			}
 			
 			for(int i=0; i<9; i++) {
-				int sum = 0;
+				int[] temp = new int[9];
 				for(int j=0; j<9; j++) {
-					sum += arr[j][i];
+					temp[(arr[i][j])-1]++; 
 				}
-				if(sum != 45) {
-					ans = 0;
+				for(int a=0; a<9; a++) {
+					if(temp[a]==0) {
+						sign = false;
+						break;
+					}
 				}
 			}
 			
-			for(int i=0; i<3; i++) {
-				for(int j=0; j<3; j++) {
-					int sum = 0;
-					for(int k=0; k<3; k++) {
-						for(int l=0; l<3; l++){
-							sum += arr[3*i+k][3*j+l];
-						}
-					}
-					if(sum != 45) {
-						ans =0;
+			for(int i=0; i<9; i++) {
+				int[] temp = new int[9];
+				for(int j=0; j<9; j++) {
+					temp[arr[j][i]-1]++; 
+				}
+				for(int a=0; a<9; a++) {
+					if(temp[a]==0) {
+						sign = false;
+						break;
 					}
 				}
 			}
-			System.out.println("#"+t+" "+ans);
+			
+			for(int i=0; i<=6; i+=3) {
+				for(int j=0; j<=6; j+=3) {
+					int[] temp = new int[9];
+					int row = i+3;
+					int column = j+3;
+					
+					for(int a=i; a<row; a++) {
+						for(int b=j; b<column; b++) {
+							temp[(arr[a][b]-1)]++;
+						}
+					}
+					for(int z=0; z<9; z++) {
+						if(temp[z]==0) {
+							sign = false;
+							break;
+						}
+					}
+				}
+			}
+			if(sign == true) {
+				System.out.println("#"+t+" 1");
+			} else {
+				System.out.println("#"+t+" 0");
+			}
+			
 		}
 	}
 }
