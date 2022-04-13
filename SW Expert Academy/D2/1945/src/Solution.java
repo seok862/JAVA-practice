6d095e5d9ab2cd63a4ac64c07147bb559f2bb9ad
SJkim <tjrwn1247@gmail.com>
2022-04-13 21:37:11 +0900

    D2 1945 다른방법

--- SW Expert Academy/D2/1945/src/Solution.java
+++ SW Expert Academy/D2/1945/src/Solution.java
@@ -4,37 +4,26 @@ public class Solution {
 		Scanner sc= new Scanner(System.in);
 		int T = sc.nextInt();
 		
+		
 		for(int t=1; t<=T; t++) {
 			int N = sc.nextInt();
-			int num2 = 0;
-			int num3 = 0;
-			int num5 = 0;
-			int num7 = 0;
-			int num11 = 0;
-			while(true) {
-				if(N%2 == 0) {
-					N = N/2;
-					num2++;
+			int[] arr = new int[11];
+			int[] a = {2,3,5,7,11};
+			while(true){
+				if(N==1)
+					break;
+				
+				for(int i=0; i<5; i++) {
+					if(N%a[i] == 0) {
+						arr[i]++;
+						N /= a[i];
+					}
 				}
-				if(N%3 == 0) {
-					N = N/3;
-					num3++;
-				}
-				if(N%5 == 0) {
-					N = N/5;
-					num5++;
-				}
-				if(N%7 == 0) {
-					N = N/7;
-					num7++;
-				}
-				if(N%11 == 0) {
-					N = N/11;
-					num11++;
-				}
-				if(N==1) break;
 			}
-			System.out.println("#"+t+" "+num2+" "+num3+" "+num5+" "+num7+" "+num11);
+			System.out.print("#"+t+" ");
+			for(int i=0; i<5; i++) {
+				System.out.print(arr[i]+" ");
+			}
 		}
 	}
 }
