054aba988d64a96b67e38211a2e36e0a900f9f99
SJkim <tjrwn1247@gmail.com>
2022-04-14 18:00:12 +0900

    D3 13038

---
+++ SW Expert Academy/D3/13038/src/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int t = 1; t <= T; t++) {
+			int N = sc.nextInt();
+			int[] arr = new int[7];
+
+			for (int i = 0; i < 7; i++) {
+				arr[i] = sc.nextInt();
+			}
+
+			int ans = Integer.MAX_VALUE;
+
+			for (int i = 0; i < 7; i++) {
+				if(arr[i] == 1) {
+					int start = i;
+					int check = 0;
+					int cnt = 0;
+
+					while (true) {
+						if (arr[start % 7] == 1) {
+							check++;
+						}
+
+						cnt++;
+						start++;
+
+						if (check == N) {
+							break;
+						}
+					}
+					ans = cnt;
+				}
+			}
+			System.out.println("#"+t+" "+ans);
+		}
+	}
+}
