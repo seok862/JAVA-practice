83d0ab23a4aba56dc44403b7d1df5b5ddb52b13c
SJkim <tjrwn1247@gmail.com>
2022-04-15 15:35:20 +0900

    D3 12368

--- SW Expert Academy/D3/12368/src/Solution.java
+++ SW Expert Academy/D3/12368/src/Solution.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
+
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
-		for(int t=1; t<=T; t++) {
-	
-				int A = sc.nextInt();
-				int B = sc.nextInt();
-				
-				if(A+B > 24) {
-					System.out.println("#"+t+" "+(A+B-24));
-				} else if(A+B == 24) {
-					System.out.println("#"+t+" "+0);
-				} else if(A+B<24) {
-					System.out.println("#"+t+" "+(A+B));
-				}
-			
+
+		for (int t = 1; t <= T; t++) {
+
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			if (A + B > 24) {
+				System.out.println("#" + t + " " + (A + B - 24));
+			} else if (A + B == 24) {
+				System.out.println("#" + t + " " + 0);
+			} else if (A + B < 24) {
+				System.out.println("#" + t + " " + (A + B));
+			}
 		}
 	}
 }
