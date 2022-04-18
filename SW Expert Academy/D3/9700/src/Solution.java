2267f949e99b847a23bae1a1b36ae51056c2ceff
SJkim <tjrwn1247@gmail.com>
2022-04-18 19:26:48 +0900

    D3 9700

---
+++ SW Expert Academy/D3/9700/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			double p = sc.nextDouble();
+			double q = sc.nextDouble();
+			
+			if(((1-p)*q) > (p*(1-q)*q)) {
+				System.out.println("#"+t+" YES");
+			} else {
+				System.out.println("#"+t+" NO");
+			}
+		}
+	}
+}
