212b3ddf9d7731062e931bef100b868a9a0ba56a
SJkim <tjrwn1247@gmail.com>
2022-04-26 21:21:31 +0900

    bronze-2 1225

--- baekjoon/bronze 2/1225/src/Main.java
+++ baekjoon/bronze 2/1225/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		String st1 = sc.next();
 		String st2 = sc.next();
 		
-		int ans = 0;
+		long ans = 0;
 		for(int i=0; i<st1.length(); i++) {
 			for(int j=0; j<st2.length(); j++) {
 				ans += Integer.parseInt(st1.substring(i, i+1))*Integer.parseInt(st2.substring(j, j+1));
