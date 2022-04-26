8291f59995b8a0acbf2acf837c3a052bde8a565a
SJkim <tjrwn1247@gmail.com>
2022-04-26 21:40:47 +0900

    bronze-1 1193

--- baekjoon/bronze 1/1193/src/Main.java
+++ baekjoon/bronze 1/1193/src/Main.java
@@ -3,23 +3,27 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int V = sc.nextInt();
+		int X = sc.nextInt();
 		
-		int cnt = 1;
-		int plus = A;
+		int cross_cnt = 1;
+		int cnt = 0;
 		
-		while(A<V) {
-			A = A-B;
-			
-			cnt++;
-			A = A+plus;
-			
-			if(A>=V) {
-				System.out.println(cnt);
-				break;
-			}
+		while(true) {
+			if(X<=cnt+cross_cnt) {
+				if(cross_cnt % 2 == 1) {
+					System.out.println(cross_cnt - (X-cnt-1)+"/"+(X-cnt));
+					break;
+				}
+				
+				else {
+					System.out.println((X-cnt)+"/"+(cross_cnt-(X-cnt-1)));
+					break;
+				}
+				
+			}else {
+				cnt += cross_cnt;
+				cross_cnt++;
+			}			
 		}
 	}
 }
