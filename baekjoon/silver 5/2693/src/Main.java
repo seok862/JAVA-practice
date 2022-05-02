d9a26e9ba7fc7bdd7fb75be0312b2ba836e747a2
SJkim <tjrwn1247@gmail.com>
2022-05-02 19:17:20 +0900

    silver-5 2693

---
+++ baekjoon/silver 5/2693/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int i=0; i<T; i++) {
+			int[] arr =new int[10];
+			
+			for(int j=0; j<10; j++) {
+				arr[j] = sc.nextInt();
+			}
+			
+			Arrays.sort(arr);
+			
+			System.out.println(arr[7]);
+		}
+		
+	}
+}
