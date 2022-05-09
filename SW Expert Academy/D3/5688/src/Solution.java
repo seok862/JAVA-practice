4069ac381cbedd0703d4030371840da7b4bc1d7f
SJkim <tjrwn1247@gmail.com>
2022-05-09 14:30:05 +0900

    D3 5688

---
+++ SW Expert Academy/D3/5688/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			long N = sc.nextLong();
+			int num = 1;
+			boolean check = false;
+			while(num*num*num <= N) {
+				if(num*num*num == N) {
+					check = true;
+					break;
+				}
+				else num++;
+			}
+			
+			if(check) {
+				System.out.println("#"+t+" "+num);
+			} else {
+				System.out.println("#"+t+" -1");
+			}
+		}
+	}
+}
