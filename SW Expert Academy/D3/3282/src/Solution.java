9011aafd236aac27ede6fd239395df31029fbbe9
SJkim <tjrwn1247@gmail.com>
2022-05-12 22:35:58 +0900

    D3 3282

---
+++ SW Expert Academy/D3/3282/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int t=1; t<=T; t++) {
+			BigInteger A = sc.nextBigInteger();
+			BigInteger B = sc.nextBigInteger();
+			
+			BigInteger ans = (A.divide(B)).multiply(A.divide(B));
+			
+			System.out.println("#"+t+" "+ans);
+		}
+	}
+}
+
+
