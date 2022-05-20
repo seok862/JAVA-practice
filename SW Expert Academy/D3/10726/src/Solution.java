3e808cb679dd50988869acf5d81f6a380b6aa032
SJkim <tjrwn1247@gmail.com>
2022-05-20 15:37:38 +0900

    D3 10726

--- SW Expert Academy/D3/10726/src/Solution.java
+++ SW Expert Academy/D3/10726/src/Solution.java
@@ -5,53 +5,28 @@ public class Solution {
 		
 		int T = sc.nextInt();
 		
-		Loop1 : for(int t=1; t<=T; t++) {
+		for(int t=1; t<=T; t++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
 			
-			int cnt = 1;
-			int a = M;
-			boolean sign = false;
-			if(M == 0 || M<=8) {
-				System.out.println("#"+t+" OFF");
-				continue Loop1;
-			}
-			while(true) {
-				a /= 2;
-				cnt++;
-				
-				if(a==1) {
+			String ans = "OFF";
+			boolean check = false;
+			for(int i=0; i<N; i++) {
+				int num = M%2;
+				if(num == 0) {
+					check = false;
 					break;
-				}
-			}
-		
-			if(cnt<N) {
-				System.out.println("#"+t+" OFF");
-				continue Loop1;
-			}
-			
-			int[] arr = new int[cnt];
-			
-			for(int i=cnt-1; i>=0; i--) {
-				arr[i] = M%2;
-				M = M/2;
-			}
-			
-			for(int i=arr.length-1; i>arr.length-N+1; i--) {
-				if(arr[i] == 1) {
-					sign = true;
-					
 				} else {
-					sign = false;
-					break;
+					check = true;
 				}
+				
+				M /= 2;
 			}
 			
-			if(sign) {
-				System.out.println("#"+t+" ON");
-			} else {
-				System.out.println("#"+t+" OFF");
-			}
+			if(check) ans="ON";
+			else ans="OFF";
+			
+			System.out.println("#"+t+" "+ans);
 		}
 	}
 }
