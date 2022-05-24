60f41a90a40f8151a02f34a0f80c253e72c98e39
SJkim <tjrwn1247@gmail.com>
2022-05-24 22:10:10 +0900

    D3 6900

--- SW Expert Academy/D3/6900/src/Solution.java
+++ SW Expert Academy/D3/6900/src/Solution.java
@@ -1,48 +1,39 @@
 import java.util.Scanner;
-import java.util.Arrays;
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);		
-		
-		int T = sc.nextInt();
-		
-		for(int t=1; t<=T; t++) {
-			int[] arr = new int[7];
-			
-			int[] sum = new int[35];
-			
-			for(int i=0; i<7; i++) {
-				arr[i] = sc.nextInt();
-			}
-			int num = 0;
-			for(int i=0; i<5; i++) {
-				for(int j=i+1; j<6; j++) {
-					for(int k=j+1; k<7; k++) {
-						sum[num] = arr[i]+arr[j]+arr[k];
-						num++;
-					}
-				}
-			}
-			Arrays.sort(sum);
-			
-			for(int i=0; i<34; i++) {
-				if(sum[i]== sum[i+1]) sum[i] = 0;
-			}
-			
-			Arrays.sort(sum);
-			
-			int ans = 0;
-			for(int i=0; i<35; i++) {
-				int cnt = 0;
-				for(int j=0; j<35; j++) {
-					if(sum[i] < sum[j]) cnt++;
-				}
-				
-				if(cnt==4) ans = sum[i];
-			}
-			
-		
-			System.out.println("#"+t+" "+ans);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         
+        int T = sc.nextInt();
+        for(int t=1; t<=T; t++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            String[] arr = new String[N];
+            int[] arr1 = new int[N];
+            for(int i=0; i<N; i++) {
+                arr[i] = sc.next();
+                arr1[i] = sc.nextInt();
+            }
+            int sum = 0;
+            for(int i=0; i<M; i++) {
+                String n = sc.next();
+             
+                 
+                for(int j=0; j<N; j++) {
+                    boolean check = true;
+                    for(int k=0; k<n.length(); k++) {
+                        if(arr[j].substring(k, k+1).equals("*")) continue;
+                         
+                        if(!arr[j].substring(k, k+1).equals(n.substring(k, k+1))) {
+                            check = false;
+                            break;
+                        }
+                    }
+                     
+                    if(check) sum += arr1[j];
+                }
+            }
+             
+            System.out.println("#"+t+" "+sum);
+        }
+    }
 }
