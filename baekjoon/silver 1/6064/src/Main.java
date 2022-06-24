8ddb6d6ad9582beae8d8e68fb882e3e77cadf839
SJkim <tjrwn1247@gmail.com>
2022-06-24 18:49:26 +0900

    silver-1 6064

---
+++ baekjoon/silver 1/6064/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int t=0; t<T; t++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int x = sc.nextInt()-1;
+			int y = sc.nextInt()-1;
+			
+			boolean flag = false;
+			
+			
+			
+			for(int i=x; i<=40001; i+=N) {				
+				if(i%M == y) {
+					System.out.println(i+1);
+					flag = true;
+					break;
+				} 
+				
+				
+			}
+			
+			if(!flag) System.out.println(-1);
+		}
+	}
+}
