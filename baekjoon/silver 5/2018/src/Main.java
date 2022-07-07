5de463da5c1913700d93cd0746dfb29860bba1fd
SJkim <tjrwn1247@gmail.com>
2022-07-07 10:11:22 +0900

    silver-5 2018

---
+++ baekjoon/silver 5/2018/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int result = 1;
+		int st = 1;
+		int end = 1;
+		int sum = 1;
+		
+		while(end != N) {
+			if(sum == N) {
+				result++;
+				end++;
+				sum += end;
+			}
+			else if(sum > N) {
+				sum -= st;
+				st++;
+			} else {
+				end++;
+				sum += end;
+			}
+		}
+		
+		System.out.println(result);
+	}
+}
